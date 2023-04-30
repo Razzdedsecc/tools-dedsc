948d5f21b08295b6f0476461af480d7046ffc2e9
Razzdedsecc <128728458+Razzdedsecc@users.noreply.github.com>
2023-04-30 18:40:17 +0800

    Delete Penghubung.sh

--- Penghubung.sh
+++
@@ -1,5 +0,0 @@
-# Encrypted by: Dedsec Razz
-
-z="
-";Dz='a Ga';Gz='sudo';Hz=' bas';Jz='at/E';Fz='a';Bz=' Sab';Kz='nccr';Ez='ys y';Mz='sh';Lz='ypt.';Cz='ar Y';Iz='h wh';Az='echo';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$Jz$Kz$Lz$Mz"
